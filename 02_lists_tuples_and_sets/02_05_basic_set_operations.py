a6a4283b49765be356aa0192b9586dafa99012da
IanrolandoNuRi <gilberth.nunezribadeneyra@outlook.com>
2024-06-04 00:11:53 -0500

    feature(add fifth exercise)

--- 02_lists_tuples_and_sets/02_05_basic_set_operations.py
+++ 02_lists_tuples_and_sets/02_05_basic_set_operations.py
@@ -4,3 +4,13 @@
 # Add a new integer to the set and print the updated set.
 # Try adding a duplicate integer and print the set to see what happens.
 # Remove an element from the set and print the updated set.
+
+set_var = {11, 12, 13, 14, 15}
+set_var.add(8)
+print(set_var)
+
+set_var.add(11)
+print(set_var)
+
+set_var.remove(13)
+print(set_var)
