41b6b198571e681edbe7731e130908a417b678ef
IanrolandoNuRi <gilberth.nunezribadeneyra@outlook.com>
2024-06-03 22:01:23 -0500

    feature(add first excercise of list)

---
+++ 02_lists_tuples_and_sets/02_01_Basic_list_operation.py
@@ -0,0 +1,8 @@
+new_list = [0, 8, 2, 6, 4, 5, 3, 7, 1, 9]
+print(f'{new_list}')
+new_list.sort()
+print(f'{new_list}')
+new_list.append(99)
+print(f'{new_list}')
+new_list.remove(2)
+print(f'{new_list}')
