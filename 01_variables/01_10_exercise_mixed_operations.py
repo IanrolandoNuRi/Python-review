e209907dc2dbb9cd1f8a0dadebaac1fa1111a972
IanrolandoNuRi <gilberth.nunezribadeneyra@outlook.com>
2024-06-10 22:17:34 -0500

    feature(add 09 file variables)

---
+++ 01_variables/01_10_exercise_mixed_operations.py
@@ -0,0 +1,26 @@
+# Exercise 1.2: Mixed Operations
+# Create variables a, b, and c and assign them the values 8, 2, and 5, respectively.
+
+a, b, c = 8, 2, 5 
+
+# Calculate and print the result of the following expressions:
+
+#a + b * c
+plus_and_prod = a + b * c
+print(plus_and_prod)
+
+#(a + b) * c
+asc_plus_and_prod = (a + b) * c
+print(asc_plus_and_prod)
+
+# a / b - c
+quotient_and_minus = a / b - c
+print(quotient_and_minus)
+
+# a % b + c
+remainder_and_plus = a % b + c
+print(remainder_and_plus)
+
+# a % b + c
+a_pow_b = a ** b
+print(a_pow_b)
