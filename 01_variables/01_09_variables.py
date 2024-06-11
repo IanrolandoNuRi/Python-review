35828c25f94ef6ef447144c140272e8ece0a7b08
IanrolandoNuRi <gilberth.nunezribadeneyra@outlook.com>
2024-06-10 22:17:23 -0500

    feature(add 09 file variables)

---
+++ 01_variables/01_09_variables.py
@@ -0,0 +1,15 @@
+#Create variables x and y and assign them the values 20 and 4, respectively.
+
+x, y = 24, 4
+
+#Calculate and print the sum, difference, product, quotient, and remainder of x and y.
+
+print("sum", x + y)
+
+print("difference", x - y)
+
+print("product", x * y)
+
+print("quotient", x / y)
+
+print("remainder", x % y)
